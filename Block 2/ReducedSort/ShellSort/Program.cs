f67f0f95bb8aa9fe4cb2c60e09b8787c6e704dd8
WingsRZyd <91160871+WingsRZyd@users.noreply.github.com>
2022-04-14 02:59:30 +0300

    Update Program.cs

--- Block 2/ReducedSort/ShellSort/Program.cs
+++ Block 2/ReducedSort/ShellSort/Program.cs
@@ -6,16 +6,6 @@ namespace ShellSort
     {
         static void Main()
         {
-            var array = new string[] {"hello", "bye", "half", "look", "hell", "helo"};
-            var sort = new ShellSort<string>();
-            array = sort.Sort(array);
-            //array = sort.Swap(array, 5, 2);
-
-            foreach (var item in array)
-            {
-                Console.WriteLine(item);
-            }
-
         }
     }
 }
